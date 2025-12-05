25a3eb33a811b6da0c020d4f0ff7e4f3cc389914
Binaya <binayaaadhikari13687@gmail.com>
2025-12-05 19:30:46 +0545

    added search

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>binaya's work</>
+    <>
+      <div className="pt-10 flex justify-center">
+        <input type="text" placeholder="Search for stories, poems and fables..." className="pl-7 text-[#726F63] w-200 h-13 bg-[#FFECC8] sticky border-2 border-black"/>
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
